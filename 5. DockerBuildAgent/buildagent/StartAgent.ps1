bb5bbf2dfae190960e0c87c04892ac6e14121396
Jakob Ehn <jakob@ehn.nu>
2019-10-14 04:41:28 +1100

    changed PAT

--- 5. DockerBuildAgent/buildagent/StartAgent.ps1
+++ 5. DockerBuildAgent/buildagent/StartAgent.ps1
@@ -5,4 +5,4 @@ param(
     [string]$tfsAgentName
 )
 
-docker run -d -m 4GB --name $tfsAgentName --storage-opt "size=50GB" -e TFS_URL=https://dev.azure.com/jakob -e TFS_PAT=7numixrk2yead7m5wmervcqd4wmlhrj5q3pod43p5ljsi5gurikq -e TFS_POOL_NAME=$tfsPoolName -e TFS_AGENT_NAME=$tfsAgentName -e SA_PASSWORD=1qaz2WSX -it buildagent
+docker run -d -m 4GB --name $tfsAgentName --storage-opt "size=50GB" -e TFS_URL=https://dev.azure.com/jakob -e TFS_PAT=5ufxc5ul3gxx3nurpcjelwxbfslw6bgrmtwhds66ndwn2fobw4na -e TFS_POOL_NAME=$tfsPoolName -e TFS_AGENT_NAME=$tfsAgentName -e SA_PASSWORD=1qaz2WSX -it buildagent
--- 5. DockerBuildAgent/buildagent/StartAgentWithVolumes.ps1
+++ 5. DockerBuildAgent/buildagent/StartAgentWithVolumes.ps1
@@ -5,4 +5,4 @@ param(
     [string]$tfsAgentName
 )
 
-docker run -d -m 4GB --name $tfsAgentName --mount type=bind,source=c:\myagent\,target=c:\setup\_work --storage-opt "size=50GB" -e TFS_URL=https://dev.azure.com/jakob -e TFS_PAT=eedpnydhyg4tilz7pxyp44fgst3wnxflde2ftlyist75i27zhsga -e TFS_POOL_NAME=$tfsPoolName -e TFS_AGENT_NAME=$tfsAgentName -e SA_PASSWORD=1qaz2WSX -v \\.\pipe\docker_engine:\\.\pipe\docker_engine -it buildagent
+docker run -d -m 4GB --name $tfsAgentName --mount type=bind,source=c:\myagent\,target=c:\setup\_work --storage-opt "size=50GB" -e TFS_URL=https://dev.azure.com/jakob -e TFS_PAT=5ufxc5ul3gxx3nurpcjelwxbfslw6bgrmtwhds66ndwn2fobw4na -e TFS_POOL_NAME=$tfsPoolName -e TFS_AGENT_NAME=$tfsAgentName -e SA_PASSWORD=1qaz2WSX -v \\.\pipe\docker_engine:\\.\pipe\docker_engine -it buildagent
