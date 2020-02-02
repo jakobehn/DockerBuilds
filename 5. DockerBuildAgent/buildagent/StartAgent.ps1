a962043feb8c78016f32118fd4ad10cf4586113b
Jakob Ehn <jakob@ehn.nu>
2020-02-02 21:25:19 +0100

    update

--- 5. DockerBuildAgent/buildagent/StartAgent.ps1
+++ 5. DockerBuildAgent/buildagent/StartAgent.ps1
@@ -5,4 +5,4 @@ param(
     [string]$tfsAgentName
 )
 
-docker run -d -m 4GB --name $tfsAgentName --storage-opt "size=50GB" -e TFS_URL=https://dev.azure.com/jakob -e TFS_PAT=5ufxc5ul3gxx3nurpcjelwxbfslw6bgrmtwhds66ndwn2fobw4na -e TFS_POOL_NAME=$tfsPoolName -e TFS_AGENT_NAME=$tfsAgentName -e SA_PASSWORD=1qaz2WSX -it buildagent
+docker run -d -m 4GB --name $tfsAgentName --storage-opt "size=50GB" -e TFS_URL=https://dev.azure.com/jakob -e TFS_PAT=fvmx2jnd2tpxgvocm5eijsefjha3dqwrclme3zftc7x34irtsg6a -e TFS_POOL_NAME=$tfsPoolName -e TFS_AGENT_NAME=$tfsAgentName -e SA_PASSWORD=1qaz2WSX -it buildagent
