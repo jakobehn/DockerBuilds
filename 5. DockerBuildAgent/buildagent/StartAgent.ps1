f1a57583ba265b2a7ca24f90eb62528853b6e41d
Jakob Ehn <jakob@ehn.nu>
2020-05-27 21:16:01 +0200

    added param for pat

--- 5. DockerBuildAgent/buildagent/StartAgent.ps1
+++ 5. DockerBuildAgent/buildagent/StartAgent.ps1
@@ -2,7 +2,8 @@ param(
     [Parameter(Mandatory=$true)]
     [string]$tfsPoolName,
     [Parameter(Mandatory=$true)]
-    [string]$tfsAgentName
+    [string]$tfsAgentName,
+    [Parameter(Mandatory=$true)]
+    [string]$pat
 )
-
-docker run -d -m 4GB --name $tfsAgentName --storage-opt "size=50GB" -e TFS_URL=https://dev.azure.com/jakob -e TFS_PAT=fvmx2jnd2tpxgvocm5eijsefjha3dqwrclme3zftc7x34irtsg6a -e TFS_POOL_NAME=$tfsPoolName -e TFS_AGENT_NAME=$tfsAgentName -e SA_PASSWORD=1qaz2WSX -it buildagent
+docker run -d -m 4GB --name $tfsAgentName --storage-opt "size=50GB" -e TFS_URL=https://dev.azure.com/jakob -e TFS_PAT=$pat -e TFS_POOL_NAME=$tfsPoolName -e TFS_AGENT_NAME=$tfsAgentName -e SA_PASSWORD=1qaz2WSX -it buildagent
--- 5. DockerBuildAgent/buildagent/StartAgentWithVolumes.ps1
+++ 5. DockerBuildAgent/buildagent/StartAgentWithVolumes.ps1
@@ -2,7 +2,9 @@ param(
     [Parameter(Mandatory=$true)]
     [string]$tfsPoolName,
     [Parameter(Mandatory=$true)]
-    [string]$tfsAgentName
+    [string]$tfsAgentName,
+    [Parameter(Mandatory=$true)]
+    [string]$pat
 )
 
-docker run -d -m 4GB --name $tfsAgentName --mount type=bind,source=c:\myagent\,target=c:\setup\_work --storage-opt "size=50GB" -e TFS_URL=https://dev.azure.com/jakob -e TFS_PAT=5ufxc5ul3gxx3nurpcjelwxbfslw6bgrmtwhds66ndwn2fobw4na -e TFS_POOL_NAME=$tfsPoolName -e TFS_AGENT_NAME=$tfsAgentName -e SA_PASSWORD=1qaz2WSX -v \\.\pipe\docker_engine:\\.\pipe\docker_engine -it buildagent
+docker run -d -m 4GB --name $tfsAgentName --mount type=bind,source=c:\myagent\,target=c:\setup\_work --storage-opt "size=50GB" -e TFS_URL=https://dev.azure.com/jakob -e TFS_PAT=$pat -e TFS_POOL_NAME=$tfsPoolName -e TFS_AGENT_NAME=$tfsAgentName -e SA_PASSWORD=1qaz2WSX -v \\.\pipe\docker_engine:\\.\pipe\docker_engine -it buildagent
